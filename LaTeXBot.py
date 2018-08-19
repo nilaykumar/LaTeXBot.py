8d54eb2c660e8ebfda329ef0a4a71479f9db3bec
Nilay Kumar <nilaykumar07@gmail.com>
2018-08-18 22:37:32 -0400

    added per-user macros, currently added/removed one at a time

--- LaTeXBot.py
+++ LaTeXBot.py
@@ -15,9 +15,12 @@ import discord
 import subprocess
 import datetime
 
-# the prefix used to invoke the bot
+# the prefix used to invoke the compiler
 # e.g. 'PREFIX \int_M C_*(-) \simeq C_*(M)'
 PREFIX = '$$'
+
+# the prefix used to run bot commands
+PREFIX_COMMAND = '!'
 
 # DPI of the output png
 DPI = 500
@@ -42,7 +45,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # if the message starts with PREFIX...
+    # if the macros file for this user doesn't exist, create an empty one
+    macroFileName = str(message.author).replace('#', '')
+    subprocess.run(['touch %s%s.tex' % (MACRO_DIR, macroFileName)], shell=True)
+
+    # if the message is latex code
     if message.content.startswith(PREFIX):
         # clean up the directory containing the temporary files
         # this will need to be modified if the bot is made async
@@ -58,6 +65,8 @@ async def on_message(message):
         messageId = message.id
         # start my stopwatch
         t1 = datetime.datetime.now()
+        # set the default TeX macros file (relative to tmp/, mind you)
+        macros = '\input{../%s%s.tex}' % (MACRO_DIR, macroFileName)
         # create a TeX file based on the template, macros, and the given latex code
         with open('%stmp%s.tex' % (TMP_DIR, messageId), 'w+') as f:
             f.write(template % (macros, latex))
@@ -73,14 +82,94 @@ async def on_message(message):
             print('SUCCESS --> Parsed [%s] from %s in %d ms' % (latex, message.author, (t2-t1).total_seconds()*1000))
             # send the final png file to Discord
             await client.send_file(message.channel, '%stmp%s.png' % (TMP_DIR, messageId))
+            return
         else:
             # something went wrong!
             # it's probably the TeX compiler whining
             # find a line starting with '!'. that's probably the error.
-            texError=((completedProcess.stdout.decode('utf-8').split('\n!'))[1].split('\n')[0].strip())
+            rawOutput = completedProcess.stdout.decode('utf-8')
+            # uncomment the next line to output the full output
+            #print(rawOutput)
+            texError = ((rawOutput.split('\n!'))[1].split('\n')[0].strip())
             print('FAILURE --> Error parsing [%s] from %s : %s ' % (latex, message.author, texError))
             # notify Discord of the error
             await client.send_message(message.channel, 'LaTeXBot ERROR >> %s' % texError)
+            return
+
+    # if the message wants to run a bot command...
+    if message.content.startswith(PREFIX_COMMAND):
+        # grab the relevant command
+        command = message.content[len(PREFIX_COMMAND):].strip()
+
+        print('%s told me to [%s].' % (message.author, command))
+
+        # parse macro-related commands
+        if command.startswith('help'):
+            # !help displays a brief description of these commands
+            await client.send_message(message.channel, 'The help feature is currently under construction.')
+            return
+        elif command.startswith('macros'):
+            # !macros manipulate the user's macros
+            macroCommand = command[6:].strip()
+            if(macroCommand == 'ls'):
+                # !macros ls displays the user's stored macros
+                # read the appropriate macros file
+                with open('%s%s.tex' % (MACRO_DIR, macroFileName), 'r') as f:
+                    macrosls = f.read().rstrip()
+                    if len(macrosls) == 0:
+                        # if the macros file is empty, notify the user
+                        await client.send_message(message.channel, '%s seems to have no macros set...' % message.author)
+                        return
+                    else:
+                        # otherwise, display the macros in a code block
+                        macroslsnumbered = '%s\'s macros:\n```\n' % message.author
+                        # display the macros with line numbers
+                        for index, s in enumerate(macrosls.split('\n')):
+                            macroslsnumbered += ('%d\t%s\n' % (index, s))
+                        macroslsnumbered += '```'
+                        # send the numbered list of macros
+                        await client.send_message(message.channel, macroslsnumbered)
+                        return
+            elif(macroCommand[0:3] == 'add'):
+                # !macros add blah appends 'blah' as a new line in the relevant macros file
+                # grab the macro to be added
+                macroToAdd = macroCommand[3:].strip()
+                if len(macroToAdd) == 0:
+                    await client.send_message(message.channel, 'Not adding a macro...')
+                    return
+                # append said macro to the macros file
+                with open('%s%s.tex' % (MACRO_DIR, macroFileName), 'a+') as f:
+                    f.write('%s\n' % macroToAdd)
+                # notify the user
+                await client.send_message(message.channel, 'Added macro `%s`.' % macroToAdd)
+                return
+            elif(macroCommand[0:2] == 'rm'):
+                # !macros rm i removes the ith macro (zero-indexed)
+                try:
+                    lineNumber = int(macroCommand[2:].strip())
+                    with open('%s%s.tex' % (MACRO_DIR, macroFileName), 'r+') as f:
+                        total = f.readlines()
+                        # abort if there are fewer macros than the requested lineNumber
+                        if (len(total) - 1 < lineNumber or lineNumber < 0):
+                            await client.send_message(message.channel, 'Could not find macro number %d!' % lineNumber)
+                            return
+                        f.seek(0)
+                        for index, s in enumerate(total):
+                            if index != lineNumber:
+                                f.write(s)
+                        f.truncate()
+                        await client.send_message(message.channel, 'Removed macro [%s].' % total[lineNumber].rstrip())
+                    return
+                except ValueError:
+                    # notify the user that they are using rm incorrectly
+                    if(macroCommand[2:].strip() == '*'):
+                        subprocess.run(['rm %s%s.tex' % (MACRO_DIR, macroFileName)], shell=True)
+                        await client.send_message(message.channel, 'Removed all of %s\'s macros.' % message.author)
+                        return
+                    await client.send_message(message.channel, 'Invalid macro number supplied!')
+                    return
+        else:
+            await client.send_message(message.channel, 'Unknown command entered.')
 
 @client.event
 async def on_ready():
@@ -93,12 +182,14 @@ async def on_ready():
 with open('%sdefault.tex' % TEMPLATE_DIR, 'r') as f:
     template = f.read()
 
-# set the default TeX macros file (from the tmp/ folder, mind you)
-macros = '\input{../%sdefault.tex}' % MACRO_DIR
-
 # read the Discord OAuth2 token
 with open('token', 'r') as f:
     token = f.readline().rstrip()
+
+# in case the relevant directories do not exist, create them
+# the template dir is expected to exist already, so we don't bother with that
+subprocess.run(['mkdir -p %s' % MACRO_DIR], shell=True, stdout=subprocess.DEVNULL)
+subprocess.run(['mkdir -p %s' % TMP_DIR], shell=True, stdout=subprocess.DEVNULL)
 
 # connect to the Discord server
 client.run(token)
--- TODO.md
+++ TODO.md
@@ -1,7 +1,8 @@
 # TODO
 
-* implement per-user macros
-* implement templates
+* allow per-user macros to be added in batch
+* implement templates (chosen via command line arguments, say)
 * normalize output sizes
-* allow per-user customization of parameters
+* allow per-user customization of parameters (?)
+* implement a !help command
 
